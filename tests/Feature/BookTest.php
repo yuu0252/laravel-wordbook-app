94a53636c6ad49ae510be9a4c3ec3f98b2938992
yuu0252 <yutanmaron@gmail.com>
2024-01-21 06:33:06 +0900

    test creating books

--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -15,6 +15,7 @@ class BookTest extends TestCase
      */
     use RefreshDatabase;
 
+    // ログインしていないユーザーはブック一覧にアクセスできない
     public function test_guest_cannot_access_books_index()
     {
         $response = $this->get(route('books.index'));
@@ -22,7 +23,8 @@ class BookTest extends TestCase
         $response->assertRedirect(route('login'));
     }
 
-    public function test_user_can_accsess_posts_index()
+    // ログインしているユーザーはブック一覧にアクセスできる
+    public function test_user_can_accsess_books_index()
     {
         $user = User::factory()->create();
         $book = Book::factory()->create(['user_id' => $user->id]);
@@ -33,6 +35,7 @@ class BookTest extends TestCase
         $response->assertSee($book->title);
     }
 
+    // ログインしていないユーザーはブック詳細ページにアクセスできない
     public function test_guest_cannot_books_show()
     {
         $user = User::factory()->create();
@@ -43,6 +46,7 @@ class BookTest extends TestCase
         $response->assertRedirect(route('login'));
     }
 
+    // ログインしているユーザーはブック詳細にアクセスできる
     public function test_user_can_access_books_show()
     {
         $user = User::factory()->create();
@@ -52,5 +56,53 @@ class BookTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertSee($book->title);
+    }
+
+    // ログインしていないユーザーはブック作成ページにアクセスできない
+    public function test_guest_cannot_access_books_create()
+    {
+        $response = $this->get(route('books.create'));
+
+        $response->assertRedirect('login');
+    }
+
+    // ログインしているユーザーはブック作成ページにアクセスできる
+    public function test_user_can_access_books_create()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('books.create'));
+
+        $response->assertStatus(200);
+    }
+
+    // ログインしていないユーザーはブックを作成できない
+    public function test_guest_cannot_access_books_store()
+    {
+        $book = [
+            'title' => 'テストブック',
+            'description' => 'テスト用のブックです。'
+        ];
+
+        $response = $this->post(route('books.store'), $book);
+
+        $this->assertDatabaseMissing('books', $book);
+        $response->assertRedirect(route('login'));
+    }
+
+    // ログインしているユーザーはブックを作成できる
+    public function test_user_can_access_books_store()
+    {
+        $user = User::factory()->create();
+
+        $book = [
+            'title' => 'テストブック',
+            'description' => 'テスト用のブックです。'
+        ];
+
+        $response = $this->actingAs($user)->post(route('books.store'), $book);
+
+        $this->assertDatabaseHas('books', $book);
+        $response->assertRedirect(route('books.index'));
     }
 }
